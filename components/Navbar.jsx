2be6b4978e416f2c4c52589f45f66432b6525324
Saim Amir <saimxamir@gmail.com>
2023-09-27 00:26:06 -0400

    update mobile sidebar view and structure

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,40 +1,115 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
 const Navbar = () => {
   return (
     <div className='fixed w-full h-20 shadow-xl z-[100]'>
       <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
-      <Image 
-      src="/assets/navLogo2.png" 
-      alt="/" 
-      width={125} height={50}/>
+        <Image src='/assets/navLogo2.png' alt='/' width={125} height={50} />
 
-    <div>
-      <ul className='hidden md:flex'>
-        <Link href='/'>
-          <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
-        </Link>
-        <Link href='/'>
-          <li className='ml-10 text-sm uppercase hover:border-b'>About</li>
-        </Link>
-        <Link href='/'>
-          <li className='ml-10 text-sm uppercase hover:border-b'>Skills</li>
-        </Link>
-        <Link href='/'>
-          <li className='ml-10 text-sm uppercase hover:border-b'>Projects</li>
-        </Link>
-        <Link href='/'>
-          <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
-        </Link>
-      </ul>
-    </div>
+        <div>
+          <ul className='hidden md:flex'>
+            <Link href='/'>
+              <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
+            </Link>
+            <Link href='/'>
+              <li className='ml-10 text-sm uppercase hover:border-b'>About</li>
+            </Link>
+            <Link href='/'>
+              <li className='ml-10 text-sm uppercase hover:border-b'>Skills</li>
+            </Link>
+            <Link href='/'>
+              <li className='ml-10 text-sm uppercase hover:border-b'>
+                Projects
+              </li>
+            </Link>
+            <Link href='/'>
+              <li className='ml-10 text-sm uppercase hover:border-b'>
+                Contact
+              </li>
+            </Link>
+          </ul>
+          <div className='md:hidden'>
+            <AiOutlineMenu size={25} />
+          </div>
+        </div>
 
+        {/* Background styling */}
+        <div className='fixed left-0 top-0 w-full h-screen bg-black/70'>
+          {/* Menu Sidebar */}
+          <div className='fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'>
+            <div>
+              {/* Top Bar of Menu */}
+              <div className='flex w-full items-center justify-between'>
+                <Image
+                  src='/assets/navLogo2.png'
+                  alt='/'
+                  width='87'
+                  height='35'
+                />
+                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+                  <AiOutlineClose />
+                </div>
+              </div>
 
+              {/* Headline */}
+              <div className='border-b border-gray-300 my-4'>
+                <p className='w-[85%] md:w-[90%] py-4'>
+                  Welcome to my personal portfolio!
+                </p>
+              </div>
+            </div>
+
+            {/* Menu List */}
+            <div className='py-4 flex-col'>
+              <ul className='uppercase'>
+                <Link href='/'>
+                  <li className='py-4 text-sm'>Home</li>
+                </Link>
+                <Link href='/'>
+                  <li className='py-4 text-sm'>About</li>
+                </Link>
+                <Link href='/'>
+                  <li className='py-4 text-sm'>Skills</li>
+                </Link>
+                <Link href='/'>
+                  <li className='py-4 text-sm'>Projects</li>
+                </Link>
+                <Link href='/'>
+                  <li className='py-4 text-sm'>Contact</li>
+                </Link>
+              </ul>
+
+              {/* Social Icons Section */}
+              <div className='pt-40 '>
+                <p className='uppercase tracking-widest text-[#000000]'>
+                  Let's Connect!
+                </p>
+
+                <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                    <FaLinkedin />
+                  </div>
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                    <FaGithub />
+                  </div>
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                    <AiOutlineMail />
+                  </div>
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                    <BsFillPersonLinesFill />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
