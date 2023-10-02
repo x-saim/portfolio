bac2b955086e8a199f8eaf0d79e343cd6ee5f62a
Saim Amir <saimxamir@gmail.com>
2023-10-02 16:46:27 -0400

    update footer for dark mode

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <footer
       id='footer'
       // style={}
-      class='bg-white rounded-lg shadow dark:bg-gray-900 m-4'
+      class='bg-white rounded-lg shadow dark:bg-gray-900 m-4 '
     >
       <div class='w-full max-w-screen-xl mx-auto p-4 md:py-8'>
         <div className=''></div>
@@ -26,7 +26,7 @@ const Footer = () => {
               Saim Amir | Portfolio
             </span>
           </a>
-          <ul class='flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400'>
+          <ul class='flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-50'>
             <li>
               <a href='/#about' class='mr-4 hover:underline md:mr-6 '>
                 About
