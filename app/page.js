62da6ce354fc030204883778631eee75992b40c4
Saim Amir <saimxamir@gmail.com>
2023-10-01 23:49:27 -0400

    add experience vertical timeline component

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import Skills from '@/components/Skills';
 import Projects from '@/components/Projects';
 import Contact from '@/components/Contact';
 import ViewMode from '@/components/viewMode';
+import Experience from '@/components/Experience';
 export default function Home() {
   return (
     <main>
@@ -13,6 +14,7 @@ export default function Home() {
       <About />
       <Skills />
       <Projects />
+      <Experience />
       <Contact />
       <ViewMode />
     </main>
--- components/Experience.jsx
+++ components/Experience.jsx
@@ -1,7 +1,92 @@
 import React from 'react';
-
-const Experience = () => {
-  return <div>Experience</div>;
-};
+import '../styles/experience.css';
+function Experience() {
+  return (
+    <section style={{ backgroundColor: '#F0F2F5' }}>
+      <div class='container py-5'>
+        <div class='main-timeline'>
+          <div class='timeline left'>
+            <div class='card'>
+              <div class='card-body p-4'>
+                <h3>2017</h3>
+                <p class='mb-0'>
+                  Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec
+                  admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea
+                  quis iuvaret expetendis his, te elit voluptua dignissim per,
+                  habeo iusto primis ea eam.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class='timeline right'>
+            <div class='card'>
+              <div class='card-body p-4'>
+                <h3>2016</h3>
+                <p class='mb-0'>
+                  Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec
+                  admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea
+                  quis iuvaret expetendis his, te elit voluptua dignissim per,
+                  habeo iusto primis ea eam.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class='timeline left'>
+            <div class='card'>
+              <div class='card-body p-4'>
+                <h3>2015</h3>
+                <p class='mb-0'>
+                  Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec
+                  admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea
+                  quis iuvaret expetendis his, te elit voluptua dignissim per,
+                  habeo iusto primis ea eam.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class='timeline right'>
+            <div class='card'>
+              <div class='card-body p-4'>
+                <h3>2012</h3>
+                <p class='mb-0'>
+                  Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec
+                  admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea
+                  quis iuvaret expetendis his, te elit voluptua dignissim per,
+                  habeo iusto primis ea eam.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class='timeline left'>
+            <div class='card'>
+              <div class='card-body p-4'>
+                <h3>2011</h3>
+                <p class='mb-0'>
+                  Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec
+                  admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea
+                  quis iuvaret expetendis his, te elit voluptua dignissim per,
+                  habeo iusto primis ea eam.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class='timeline right'>
+            <div class='card'>
+              <div class='card-body p-4'>
+                <h3>2007</h3>
+                <p class='mb-0'>
+                  Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec
+                  admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea
+                  quis iuvaret expetendis his, te elit voluptua dignissim per,
+                  habeo iusto primis ea eam.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
 
 export default Experience;
