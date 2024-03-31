c7309d02f2c21ea4ab8336a8334fd71e52bbe5cf
Saim Amir <saimxamir@gmail.com>
2024-03-31 02:33:43 -0400

    add resume link to landing page

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -11,8 +11,12 @@ const Main = () => {
         {/* Project Link Container*/}
         <div className='uppercase max-w-[600px] rounded-full px-3 py-1 text-md leading-6 text-gray-600 dark:text-black dark:text-md ring-1 ring-gray-900/10 hover:ring-gray-900/20 dark:ring-gray-50/10 dark:hover:ring-gray-50/20 tracking-widest bg-gray-100 shadow-md dark:shadow-gray-400 font-semibold '>
           Check out my{' '}
+          <Link href='/resume' className='font-semibold text-[#AD954C] '>
+            Resume <span aria-hidden='true'></span>
+          </Link>
+          &{' '}
           <Link href='/#projects' className='font-semibold text-[#AD954C] '>
-            Projects <span aria-hidden='true'>&rarr;</span>
+            Projects <span aria-hidden='true'></span>
           </Link>
         </div>
 
